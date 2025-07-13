d7e1f84378f2b7b3790176d4f30f8411f5866cc5
Gabriel Zook <gabyzook@gmail.com>
2025-07-12 21:00:33 -0700

    fixing annoyancses

--- src/components/DesktopSplash.tsx
+++ src/components/DesktopSplash.tsx
@@ -206,7 +206,7 @@ const DesktopSplash: React.FC = () => {
           src="/optimized/coffee_cup-400.webp"
           alt="Coffee Cup"
           className="absolute pointer-events-none"
-          style={{ left: '-5vw', top: '-5vw', width: '30vw', zIndex: 101 }}
+          style={{ left: '-15vw', top: '-15vw', width: '30vw', zIndex: 101 }}
           srcSet="/optimized/coffee_cup-400.webp 400w, /optimized/coffee_cup-800.webp 800w, /optimized/coffee_cup-1200.webp 1200w"
           sizes="(max-width: 600px) 100vw, 50vw"
         />
