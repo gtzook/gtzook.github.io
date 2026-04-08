7df7f05fcd2eec0d98186fb84c57ad1d17b8bd64
Gabriel Zook <43184112+gtzook@users.noreply.github.com>
2026-04-08 12:49:34 -0700

    Update BookGallery.tsx

--- src/components/BookGallery.tsx
+++ src/components/BookGallery.tsx
@@ -104,7 +104,7 @@ const BookGallery: React.FC = () => {
               width: '80px',
               height: '80px',
               fontSize: '36px',
-              marginRight: '120px',
+              marginRight: '0px',
               background: 'rgba(0,0,0,0.4)',
               color: 'white',
               borderRadius: '9999px',
