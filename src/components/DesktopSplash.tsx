e009fa24accda86f882c58ded2cae39dc96d27ca
Gabriel Zook <43184112+gtzook@users.noreply.github.com>
2026-04-08 14:06:43 -0700

    Update DesktopSplash.tsx

--- src/components/DesktopSplash.tsx
+++ src/components/DesktopSplash.tsx
@@ -37,10 +37,19 @@ const DesktopSplash: React.FC = () => {
     });
   };
       return (
+        <div
+        style={{
+          height: '100vh',        // limit splash to viewport height
+          overflow: 'hidden',     // hide vertical overflow
+          position: 'relative',
+        }}
+      >
     <div
         style={{
           width: '100%',
+          height: '100%',
           overflowX: 'auto',
+          overflowY: 'hidden',
       
           // ✅ background fills entire page height naturally
           backgroundImage: 'url(/splash_bg.jpg)',
@@ -55,7 +64,6 @@ const DesktopSplash: React.FC = () => {
           className="relative"
           style={{
             width: `${DESIGN_WIDTH}px`,
-            minWidth: `${DESIGN_WIDTH}px`,
             position: 'relative',
             margin: '0 auto',
             zIndex: 1,
@@ -259,6 +267,7 @@ const DesktopSplash: React.FC = () => {
         </div>
       <div style={{ height: `${DESIGN_HEIGHT}px` }} />
       </section>
+    </div>
     </div>
   );
 };
