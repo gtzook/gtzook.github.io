dad66c2abec45afd671854cee6b41b83bd61dd88
Gabriel Zook <43184112+gtzook@users.noreply.github.com>
2026-04-08 12:34:07 -0700

    Increase margin-right in BookGallery component

--- src/components/BookGallery.tsx
+++ src/components/BookGallery.tsx
@@ -104,7 +104,7 @@ const BookGallery: React.FC = () => {
               width: '80px',
               height: '80px',
               fontSize: '36px',
-              marginRight: '80px',
+              marginRight: '120px',
               background: 'rgba(0,0,0,0.4)',
               color: 'white',
               borderRadius: '9999px',
